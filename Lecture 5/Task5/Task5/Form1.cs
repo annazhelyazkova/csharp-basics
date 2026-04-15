41ed39632df3efba5d2a480802f139c3c4a2f760
Anna Zhelyazkova <anna.s.zhelyazkova@gmail.com>
2026-04-15 23:38:51 +0300

    Updating repo

---
+++ Lecture 5/Task5/Task5/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void increace(ref int a)
+        {
+            a = 5000;
+            textBox1.Text = a.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int salary = 3000;
+            increace(ref salary);
+            textBox1.Text = salary.ToString();
+        }
+    }
+}
---
+++ Lecture 5/Task7/Task7/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            int factorial (int n)
+            {
+                int i, f = 1;
+                for (i = 0; i <= n; i++) f *= i;
+                return f;
+            }
+
+            int number, result;
+            number = int.Parse(textBox1.Text);
+            result = factorial(number);
+            label1.Text = result.ToString();
+            */
+
+            int factorial(int n)
+            {
+                if (n == 0) return 1;
+                else return n*factorial(n-1);
+            }
+
+            int number, result;
+            number = int.Parse(textBox1.Text);
+            result = factorial(number);
+            label1.Text = result.ToString();
+            
+        }
+    }
+}
